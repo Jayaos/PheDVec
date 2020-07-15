880a1fb9519ab1a5c3b180edec95c5594dfc1bbf
Jay Lee <jayaos1002@gmail.com>
2020-07-14 20:22:55 -0400

    package update

--- src/GloVe.py
+++ src/GloVe.py
@@ -6,65 +6,57 @@ import random
 import sys
 import os
 import pickle
+from dotmap import DotMap
+import json
 from collections import defaultdict
 
 class GloVe(tf.keras.Model):
-    def __init__(self, input_path, output_path, embedding_dim=256, max_vocab_size=100, 
-    scaling_factor=0.75, batch_size=512, learning_rate=0.01):
+    def __init__(self, json_dir, max_vocab_size=100, scaling_factor=0.75, batch_size=512):
         super(GloVe, self).__init__()
-        self.input_path = input_path
-        self.output_path = output_path
-        
-        self.embedding_dim = embedding_dim
+        self.config = set_config(json_dir)
+
         self.max_vocab_size = max_vocab_size
         self.scaling_factor = scaling_factor
         self.batch_size = batch_size
-        self.vocab_size = 0
-        self.concept2id = None
+        self.training_data = None        
+        self.concept2id = load_dictionary(self.config.data.concept2id)
+        self.vocab_size = len(self.concept2id)
         self.comap = None
         self.comatrix = None
-        self.optimizer = tf.keras.optimizers.Adagrad(learning_rate=learning_rate)
+        self.optimizer = tf.keras.optimizers.Adagrad(learning_rate=self.config.hparams.learning_rate)
         self.epoch_loss_avg = []
 
-    def build_dict(self, corpus):
-        tokenizer = tf.keras.preprocessing.text.Tokenizer()
-        tokenizer.fit_on_texts(corpus)
-        self.concept2id = tokenizer.word_index
-        self.vocab_size = len(self.concept2id)
-
-    def save_dict(self, save_dir):
-        with open(save_dir + "/concept2id.pkl", "wb") as f:
-            pickle.dump(self.concept2id, f)
-        print("concept2id successfully saved in the savedir")
+    def loadData(self):
+        print("load training data...")
+        self.training_data = load_data(self.config.data.training_data_phedvec)[0]
      
-    def fit_to_corpus(self, corpus):
+    def buildCoMatrix(self):
         self.comap = defaultdict(float)
-        self.comatrix = np.zeros((self.vocab_size+1, self.vocab_size+1), dtype=np.float64)
-        concept2id = self.concept2id
+        self.comatrix = np.zeros((self.vocab_size, self.vocab_size), dtype=np.float64)
 
-        for i in tqdm(range(len(corpus))):
-            patient = corpus[i]
-            for p in patient:
-                for k in patient:
+        for i in tqdm(range(len(self.training_data))):
+            record = self.training_data[i]
+            for p in record:
+                for k in record:
                     if p != k:
                         self.comap[(p, k)] += 1
         
         for pair, count in self.comap.items():
-            self.comatrix[concept2id[pair[0]], concept2id[pair[1]]] = count
+            self.comatrix[self.concept2id[pair[0]], self.concept2id[pair[1]]] = count
 
-    def init_params(self):
+    def initParams(self):
         with tf.device("/cpu:0"):
             """must be implemented with cpu-only env since this is sparse updating"""
-            self.target_embeddings = tf.Variable(tf.random.uniform([self.vocab_size+1, self.embedding_dim], 0.1, -0.1),
+            self.target_embeddings = tf.Variable(tf.random.uniform([self.vocab_size, self.embedding_dim], 0.1, -0.1),
                                                  name="target_embeddings")
-            self.context_embeddings = tf.Variable(tf.random.uniform([self.vocab_size+1, self.embedding_dim], 0.1, -0.1),
+            self.context_embeddings = tf.Variable(tf.random.uniform([self.vocab_size, self.embedding_dim], 0.1, -0.1),
                                                   name="context_embeddings")
-            self.target_biases = tf.Variable(tf.random.uniform([self.vocab_size+1], 0.1, -0.1),
+            self.target_biases = tf.Variable(tf.random.uniform([self.vocab_size], 0.1, -0.1),
                                              name='target_biases')
-            self.context_biases = tf.Variable(tf.random.uniform([self.vocab_size+1], 0.1, -0.1),
+            self.context_biases = tf.Variable(tf.random.uniform([self.vocab_size], 0.1, -0.1),
                                               name="context_biases")
 
-    def compute_cost(self, x):
+    def computeCost(self, x):
         with tf.device("/gpu:0"):
             """x = [target_ind, context_ind, co_occurrence_count]"""
             target_emb = tf.nn.embedding_lookup([self.target_embeddings], x[0])
@@ -85,7 +77,7 @@ class GloVe(tf.keras.Model):
           
         return batch_cost
 
-    def compute_gradients(self, x):
+    def computeGradients(self, x):
         with tf.GradientTape() as tape:
             cost = self.compute_cost(x)
         return cost, tape.gradient(cost, self.trainable_variables)
@@ -105,28 +97,28 @@ class GloVe(tf.keras.Model):
         assert len(i_ids) == len(co_occurs), "The length of the data are not the same"
         return i_ids, j_ids, co_occurs
 
-    def get_embeddings(self):
+    def getEmbeddings(self):
         self.embeddings = self.target_embeddings + self.context_embeddings
     
-    def save_embeddings(self, save_dir, epoch, avg_loss):
+    def saveEmbeddings(self, save_dir, epoch, avg_loss):
         self.get_embeddings()
         np.save(os.path.join(save_dir, "glove_emb_e{:03d}_loss{:.4f}.npy".format(epoch, avg_loss)),
                 self.embeddings)
         print("Embedding results have been saved")
 
-    def train_GloVe(self, num_epochs, save_dir):
-        i_ids, j_ids, co_occurs = self.prepare_batch()
+    def train(self, num_epochs, batch_size):
+        i_ids, j_ids, co_occurs = prepare_trainingset(self.comap)
         total_batch = int(np.ceil(len(i_ids) / self.batch_size))
         cost_avg = tf.keras.metrics.Mean()
 
         for epoch in range(num_epochs):
             progbar = tf.keras.utils.Progbar(len(i_ids))
             
-            for i in range(total_batch):
+            for i in random.sample(range(total_batch), total_batch): # shuffling the data 
                 i_batch = i_ids[i * self.batch_size : (i+1) * self.batch_size]
                 j_batch = j_ids[i * self.batch_size : (i+1) * self.batch_size]
                 co_occurs_batch = co_occurs[i * self.batch_size : (i+1) * self.batch_size]
-                cost, gradients = self.compute_gradients([i_batch, j_batch, co_occurs_batch])
+                cost, gradients = self.computeGradients([i_batch, j_batch, co_occurs_batch])
                 self.optimizer.apply_gradients(zip(gradients, self.trainable_variables))
                 cost_avg(cost) 
                 progbar.add(self.batch_size)
@@ -137,13 +129,40 @@ class GloVe(tf.keras.Model):
                 print("Epoch {}: Loss: {:.4f}".format(epoch, avg_loss))
                 self.epoch_loss_avg.append(avg_loss)
                     
-        self.save_embeddings(save_dir, epoch, avg_loss)
+        self.saveEmbeddings(save_dir, epoch, avg_loss)
 
-def open_patient_record(data_dir):
-    patient_record = []
-    with open(data_dir, "r") as f:
-        patients = f.read().split("\n")
-        for i in tqdm(range(len(patients))):
-            patient = patients[i]
-            patient_record.append(patient.split(","))
-    return patient_record
+def set_config(json_file):
+    """
+    Get the config from a json file
+    """
+    # parse the configurations from the config json file provided
+    with open(json_file, 'r') as config_file:
+        config_dict = json.load(config_file)
+    # convert the dictionary to a namespace using bunch lib
+    config = DotMap(config_dict)
+    return config
+
+def load_dictionary(data_dir):
+    with open(data_dir, 'rb') as f:
+        my_dict = pickle.load(f)
+    return my_dict
+
+def prepare_trainingset(comap):
+    i_ids = []
+    j_ids = []
+    co_occurs = []
+    comap_list = list(comap.items())
+
+    for pair, co_occur in comap_list:
+        i_ids.append(self.concept2id[pair[0]])
+        j_ids.append(self.concept2id[pair[1]])
+        co_occurs.append(co_occur)
+     
+    assert len(i_ids) == len(j_ids), "The length of the data are not the same"
+    assert len(i_ids) == len(co_occurs), "The length of the data are not the same"
+    return i_ids, j_ids, co_occurs
+
+def load_data(data_dir):
+    with open(data_dir, "rb") as f:
+        mylist = pickle.load(f)
+    return mylist
